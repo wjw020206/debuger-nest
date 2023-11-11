a1deeac0349698dec84e0136d823dbb40d02123f
CodePencil <jiawei.wang2021@gmail.com>
2023-11-11 12:14:36 +0800

    refactor(标签模块): 删除了标签模块部分无用的代码

--- src/tag/tag.controller.ts
+++ src/tag/tag.controller.ts
@@ -1,25 +1,9 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-  Query
-} from '@nestjs/common';
+import { Controller, Get, Param, Delete, Query } from '@nestjs/common';
 import { TagService } from './tag.service';
-import { CreateTagDto } from './dto/create-tag.dto';
-import { UpdateTagDto } from './dto/update-tag.dto';
 
 @Controller('tag')
 export class TagController {
   constructor(private readonly tagService: TagService) {}
-
-  @Post()
-  create(@Body() createTagDto: CreateTagDto) {
-    return this.tagService.create(createTagDto);
-  }
 
   @Get()
   findAll(
@@ -33,11 +17,6 @@ export class TagController {
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.tagService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateTagDto: UpdateTagDto) {
-    return this.tagService.update(+id, updateTagDto);
   }
 
   @Delete(':id')
--- src/tag/tag.service.ts
+++ src/tag/tag.service.ts
@@ -1,20 +1,14 @@
 import { Injectable } from '@nestjs/common';
-import { CreateTagDto } from './dto/create-tag.dto';
-import { UpdateTagDto } from './dto/update-tag.dto';
 import { PrismaService } from 'src/common/prisma.service';
 
 @Injectable()
 export class TagService {
   constructor(private prisma: PrismaService) {}
 
-  create(createTagDto: CreateTagDto) {
-    return 'This action adds a new tag';
-  }
-
   async findAll(
     page = 1,
     search?: string,
-    method: 'popular' | 'letter' | 'latest' = 'popular'
+    method?: 'popular' | 'letter' | 'latest'
   ) {
     const pageCount = 36;
 
@@ -116,10 +110,6 @@ export class TagService {
 
   findOne(id: number) {
     return `This action returns a #${id} tag`;
-  }
-
-  update(id: number, updateTagDto: UpdateTagDto) {
-    return `This action updates a #${id} tag`;
   }
 
   remove(id: number) {
